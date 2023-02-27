67a3b062a451307a503db8707eb90c2cfa33153c
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-02-27 12:26:44 +0100

    Fase disseny

--- src/main/java/cat/copernic/copernicjobs/model/Incidencia.java
+++ src/main/java/cat/copernic/copernicjobs/model/Incidencia.java
@@ -5,20 +5,36 @@
  */
 package cat.copernic.copernicjobs.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import java.time.LocalDate;
 import lombok.Data;
-import lombok.NonNull;
 
 /**
  *
  * @author joang
  */
 @Data
+@Entity
 public class Incidencia{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, unique = true)
     private int id;
-    private String titulo;
+    @Column(name = "descripcion")
     private String descripcion;
+    @Column(name = "fechaIncidencia")
     private LocalDate fechaIncidencia;
+    @Column(name = "estado")
     private int estado;
+    
+    @OneToOne
+    @JoinColumn(name="usuario_id")
+    private Usuario usuario;
     
 }
--- src/main/java/cat/copernic/copernicjobs/model/Persona.java
+++ src/main/java/cat/copernic/copernicjobs/model/Persona.java
@@ -4,6 +4,8 @@
  */
 package cat.copernic.copernicjobs.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import java.sql.Date;
 import java.time.LocalDate;
 import lombok.Data;
@@ -15,14 +17,33 @@ import lombok.NonNull;
  */
 
 @Data
+@Entity
 public class Persona extends Usuario{
 
-    protected String nombre;
-    protected String codPostal;
-    protected String municipio;
-    protected String direccion;
-    protected String movil;
-    protected int sexo;
-    protected String apellidos;
-    protected String correoContacto;
+    @Column(name = "nombre")
+    private String nombre;
+    
+    @Column(name = "cod_postal")
+    private String codPostal;
+    
+    @Column(name = "municipio")
+    private String municipio;
+    
+    @Column(name = "direccion")
+    private String direccion;
+    
+    @Column(name = "movil")
+    private String movil;
+    
+    @Column(name = "sexo")
+    private int sexo;
+    
+    @Column(name = "sexo_desc")
+    private String sexoDesc;
+    
+    @Column(name = "apellidos")
+    private String apellidos;
+    
+    @Column(name = "correo_contacto")
+    private String correoContacto;
 }
