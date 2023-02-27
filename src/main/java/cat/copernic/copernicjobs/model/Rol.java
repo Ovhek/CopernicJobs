93c4669612c3f947567370953079911d3246674e
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-02-27 13:50:29 +0100

    Tasca 6A. Accés a la base de dades. Interfícies DAO i classes Model Administrador, modulo, rol y noticia.

--- src/main/java/cat/copernic/copernicjobs/model/Rol.java
+++ src/main/java/cat/copernic/copernicjobs/model/Rol.java
@@ -4,7 +4,11 @@
  */
 package cat.copernic.copernicjobs.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.NonNull;
 
@@ -13,8 +17,12 @@ import lombok.NonNull;
  * @author Cole
  */
 @Data
+@Entity
 public class Rol {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false, unique = true)
     private int ID;
+    @Column(name = "nom")
     private String nom;
 }
