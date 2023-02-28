fa755f01af8c9303155cc5c68122e828fe5e6ef4
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-02-28 11:41:51 +0100

    Merge origin/Albert into Joan
    Conflicts:
    	pom.xml

---
+++ src/main/java/cat/copernic/copernicjobs/DAO/OfertaDAO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.copernic.copernicjobs.DAO;
+
+import cat.copernic.copernicjobs.model.Oferta;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Albert
+ */
+public interface OfertaDAO extends CrudRepository<Oferta,Integer>{ 
+    
+    //Aqui podrem afegir altres mètodes que necessitem i que no estiguin definits a la interface CrudRepository,
+    //com el següent mètode, findByNom que retornarà els gossos amb el nom passat per paràmetre
+    List<Oferta> findByNom(String nom);
+
+}
