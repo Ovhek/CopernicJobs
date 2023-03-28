6fa7ee1b675ae7ad613a62c974c98577c735f4f4
Alexandru Ioan Cruceat - <cruceat.alexandru.ioan@alumnat.copernic.cat>
2023-03-28 16:43:54 +0000

    Merge Alex a Develop

--- src/main/java/cat/copernic/copernicjobs/alumno/controladores/MisInscripciones.java
+++ src/main/java/cat/copernic/copernicjobs/alumno/controladores/MisInscripciones.java
@@ -6,16 +6,16 @@ package cat.copernic.copernicjobs.alumno.controladores;
 
 import cat.copernic.copernicjobs.administrador.servicios.AdministradorService;
 import cat.copernic.copernicjobs.alumno.servicios.AlumnoService;
-import cat.copernic.copernicjobs.dao.InscripcionDAO;
 import cat.copernic.copernicjobs.alumno.servicios.InscripcionService;
 import cat.copernic.copernicjobs.general.utils.CargarPantallaPrincipal;
 import cat.copernic.copernicjobs.general.utils.NavBarType;
-import cat.copernic.copernicjobs.model.Alumno;
-import cat.copernic.copernicjobs.model.Empresa;
+import cat.copernic.copernicjobs.model.Inscripcion;
 import cat.copernic.copernicjobs.model.Oferta;
 import cat.copernic.copernicjobs.model.Usuario;
-import java.util.ArrayList;
-import java.util.Arrays;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,6 +23,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
  *
@@ -33,26 +34,44 @@ public class MisInscripciones {
 
     @Autowired
     InscripcionService inscripcionService;
-    
+
     @Autowired
     AlumnoService alumnoService;
 
     @Autowired
     AdministradorService administradorService;
-    
+
     @PreAuthorize("hasAuthority('alumne')")
     @GetMapping("/alumne/inscripcions")
-    public String inicio(Model model, @AuthenticationPrincipal UserDetails username) {
-
+    public String inicio(Model model, @AuthenticationPrincipal UserDetails username, HttpServletRequest request) {
+        Map<String, ?> inputFlashMap = RequestContextUtils.getInputFlashMap(request);
         Usuario alumno = alumnoService.buscarAlumnoPorUsername(username.getUsername());
-        
-        if(alumno == null) alumno = administradorService.buscarAdministradorPorUsername(username.getUsername());
-        
+
+        if (alumno == null) {
+            alumno = administradorService.buscarAdministradorPorUsername(username.getUsername());
+        }
+
+        List<Inscripcion> inscripciones = inscripcionService.buscarInscripcionPorAlumnoId(alumno.getId());
+
+        if (inputFlashMap != null) {
+            List<Oferta> ofertas = (List<Oferta>) inputFlashMap.get("ofertas");
+            inscripciones = inscripciones.stream()
+                    .filter(inscripcion -> {
+                        for (Oferta oferta : ofertas) {
+                            if (inscripcion.getOferta().getId() == oferta.getId()) {
+                                return true;
+                            }
+                        }
+                        return false;
+                    })
+                    .collect(Collectors.toList());
+        }
+
         //Ruta donde está el archivo html 
         String ruta = "alumno/";
         //nombre del archivo html
         String archivo = "misInscripciones";
-        model.addAttribute("inscripciones", inscripcionService.buscarInscripcionPorAlumnoId(alumno.getId()));
+        model.addAttribute("inscripciones", inscripciones);
         //Cargamos el archivo y lo añadimos a la plantilla de la página principal
         return CargarPantallaPrincipal.cargar(model, NavBarType.ALUMNO, ruta, archivo, "Les meves inscripcions", username);
     }
