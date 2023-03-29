345c5d07f9a13521b8a418e41090e647e58982ad
Joan Galindo <galindo.palacio.joan@alumnat.copernic.cat>
2023-03-29 17:16:31 +0200

    Tasca 9. Aplicar Bootstrap i Font Awesome modul Administrador acabat + fet funcionalitat de validacions. Codificació finalitzada

--- src/main/java/cat/copernic/copernicjobs/administrador/controladores/VerOfertasAdmin.java
+++ src/main/java/cat/copernic/copernicjobs/administrador/controladores/VerOfertasAdmin.java
@@ -5,11 +5,14 @@
  */
 package cat.copernic.copernicjobs.administrador.controladores;
 
+import cat.copernic.copernicjobs.empresa.servicios.EmpresaService;
 import cat.copernic.copernicjobs.empresa.servicios.OfertaService;
 import cat.copernic.copernicjobs.general.utils.CargarPantallaPrincipal;
 import cat.copernic.copernicjobs.general.utils.NavBarType;
 import cat.copernic.copernicjobs.model.Empresa;
 import cat.copernic.copernicjobs.model.Oferta;
+import jakarta.servlet.http.HttpServletRequest;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
@@ -30,16 +36,20 @@ public class VerOfertasAdmin {
     @Autowired
     private OfertaService ofertaService;
 
+    @Autowired
+    EmpresaService empresaService;
+    
     @PreAuthorize("hasAuthority('administrador')")
     @GetMapping("/verOfertasAdmin/{id}")
-    public String inicio(Model model, @AuthenticationPrincipal UserDetails username) {
-
+    public String inicio(Empresa empresa, Model model, @AuthenticationPrincipal UserDetails username) {
+        
+        Empresa emp = empresaService.cercarEmpresa(empresa);
         //Ruta donde está el archivo html 
         String ruta = "administrador/";
         //nombre del archivo html
         String archivo = "verOfertasAdmin";
 
-        model.addAttribute("ofertas", ofertaService.llistarOfertas());
+        model.addAttribute("ofertas", ofertaService.listarPorNombre(emp.getNombreEmpresa()));
         //Cargamos el archivo y lo añadimos a la plantilla de la página principal
         return CargarPantallaPrincipal.cargar(model, NavBarType.ADMINISTRADOR, ruta, archivo, "Inici", username);
     }
@@ -57,4 +67,12 @@ public class VerOfertasAdmin {
         return CargarPantallaPrincipal.cargar(model, NavBarType.ADMINISTRADOR, ruta, archivo, "Inici", username);
     }
 
+    @PostMapping("/buscarOferta/{id}")
+    public String buscarOferta(@RequestParam(name = "buscar") String btnValue,@RequestParam(name = "search-input") String buscar,@RequestParam(name="sort-select") String ordenar, @AuthenticationPrincipal UserDetails username,Model model, HttpServletRequest request,RedirectAttributes redirectAttributes, Empresa empresa){
+        Empresa emp = empresaService.cercarEmpresa(empresa);
+        
+        model.addAttribute("ofertas",ofertaService.filtrarOfertasOrdenacion(buscar, ordenar, emp.getNombreEmpresa()));
+        
+        return "redirect:/verOfertasAdmin/{id}";
+    }
 }
